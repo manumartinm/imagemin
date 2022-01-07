12aeadc3c555a36aee8c41f504dd3592597e582d
Manuel Martin Morante <manuzombiesbf1@gmail.com>
2022-01-07 07:03:21 +0100

    Make the CLI tool beautiful

--- main.js
+++ main.js
@@ -1,19 +1,50 @@
-import imagemin from 'imagemin'
-import imageminJpegtran from 'imagemin-jpegtran'
-import imageminMozjpeg from 'imagemin-mozjpeg'
+import chalk from 'chalk';
+import figlet from 'figlet';
+import inquirer from 'inquirer';
+import imagemin from 'imagemin';  
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminMozjpeg from 'imagemin-mozjpeg';
 
-const quality = process.argv[2] || 70
-
-try {
-	await imagemin(['./images/*.{jpg,png}'], {
-		destination: './optimized',
-		plugins: [
-			imageminJpegtran(),
-			imageminMozjpeg({
-				quality
-			})
-		]
-	})	
-} catch (e) {
-	console.error(e)
+const cliMessage = (message) => {
+  console.log(chalk.bold.cyan(figlet.textSync(message, { 
+    font: 'ANSI Shadow',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  })))
 }
+
+const queryParams = () => {
+  const qs = [
+    {
+      name: 'quality',
+      type: 'input',
+      message: 'Selecciona la calidad de las imagenes de 0 a 100',
+    },
+  ]
+  return inquirer.prompt(qs)
+};
+
+const optimize = async (data) => {
+  const filesPath = `images/*.{jpg, jpeg, png}`
+	try {
+		await imagemin([filesPath], {
+			destination: './optimized',
+			plugins: [
+				imageminJpegtran(),
+				imageminMozjpeg({
+					quality: parseInt(data.quality)
+				})
+			]
+		})
+    console.log(`
+      ------ OPTIMIZADAS CORRECTAMENTE ------\n
+      Tu servidor te lo va a agradecer.
+      ---------------------------------------\n
+    `)
+	} catch (e) {
+		chalk.red.bold(`Ha habido un error al optimizar las imagenes: ${e}`)
+	}
+}
+
+cliMessage('OPTIMIZE IMAGES');
+optimize(await queryParams());
